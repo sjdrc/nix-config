4602522e7853536b613d91f1a5687ffa24f31151
Sebastien Collier <s@sjc3.me>
2025-09-24 12:21:37 +1000

    Disable greetd extra unit options

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -39,14 +39,14 @@
     };
   };
 
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
-  };
+  #systemd.services.greetd.serviceConfig = {
+  #  Type = "idle";
+  #  StandardInput = "tty";
+  #  StandardOutput = "tty";
+  #  StandardError = "journal";
+  #  # Without these bootlogs will spam on screen
+  #  TTYReset = true;
+  #  TTYVHangup = true;
+  #  TTYVTDisallocate = true;
+  #};
 }
