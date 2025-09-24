bfb40c3cc250e56ee094019ae8d9d23ad6cbced3
Sebastien Collier <s@sjc3.me>
2025-09-24 11:16:20 +1000

    Re-add quiet boot options and move tuigreet to boot module

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -10,16 +14,39 @@
   ];
 
   # Graphical boot
-  #boot.plymouth.enable = true;
+  boot.plymouth.enable = true;
 
   # Quiet boot
-  #boot.initrd.verbose = false;
-  #boot.consoleLogLevel = 0;
-  #boot.initrd.systemd.enable = true;
-  #boot.kernelParams = [
-  #  "quiet"
-  #  "splash"
-  #  "systemd.show_status=auto"
-  #  "rd.udev.log_level=3"
-  #];
+  boot.initrd.verbose = false;
+  boot.consoleLogLevel = 0;
+  boot.initrd.systemd.enable = true;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "systemd.show_status=auto"
+    "rd.udev.log_level=3"
+  ];
+
+  # Display Manager
+  services.greetd = {
+    enable = true;
+    greeterManagesPlymouth = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --user-menu --time --remember --asterisks --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
+        user = "greeter";
+      };
+    };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
 }
