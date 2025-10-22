a99a682bef4cb27ae9e84d1417a82cff39ca97c8
Sebastien Collier <s@sjc3.me>
2025-10-23 05:25:56 +1100

    Add desktop portal fix for niri (not currently working)

--- modules/programs/niri.nix
+++ modules/programs/niri.nix
@@ -13,6 +13,31 @@
     wl-clipboard
     wayland-utils
   ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+    config = {
+      common = {
+        default = ["gtk"];
+      };
+      niri = {
+        default = [
+          "gtk"
+          "gnome"
+        ];
+        "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
+        "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
+      };
+    };
+  };
+  environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "niri";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "niri";
+  };
   home-manager.users.sebastien = {config, ...}: {
     services = {
       swaync.enable = true;
