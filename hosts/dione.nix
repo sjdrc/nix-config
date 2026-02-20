825545468fc5aeecd4336dda6c238a65dc5304d4
Sebastien Collier <s@sjc3.me>
2026-02-20 17:37:23 +1100

    Add data disk to dione (for media) commented out until copy is completed

--- hosts/dione.nix
+++ hosts/dione.nix
@@ -8,6 +8,14 @@
   custom.hardware.cpu.intel.enable = true;
   custom.hardware.gpu.nvidia.enable = true;
 
+  # Data SSD
+  # sudo e2label <disk> data
+  #fileSystems."/data" = {
+  #  label = "data";
+  #  fsType = "ext4";
+  #  options = ["noatime" "nodiratime" "discard"];
+  #};
+
   # Profiles
   custom.profiles.desktop.enable = true;
   custom.profiles.development.enable = true;
